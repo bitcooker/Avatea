cfb29553b3fad7452c25abd1f60446d868d223fe
A <albertsamseer@MacBook-Air-van-Albert.local>
2022-05-11 17:54:44 +0200

    Fix project pages

---
+++ pages/projects/[slug].js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import {useWallet} from "use-wallet";
+import {useState, useEffect} from "react";
+
+export default function ProjectDetail() {
+
+    const wallet = useWallet();
+
+    return (
+        <div>
+            <h1>Project Detail page Page</h1>
+        </div>
+    )
+}
---
+++ pages/projects/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import {useWallet} from "use-wallet";
+import {useState, useEffect} from "react";
+
+export default function Projects() {
+
+    const wallet = useWallet();
+
+    return (
+        <div>
+            <h1>Project List Page</h1>
+        </div>
+    )
+}
