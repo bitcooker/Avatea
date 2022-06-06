0fc7075a261eb4453fba223fb4ae934f293cfdfa
bitcooker <dreamhighpassionhigh@gmail.com>
2022-06-06 20:49:20 +0200

    Add customize style

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,17 @@ module.exports = {
       fontFamily: {
         poppins: ["Poppins"],
       },
+      height: {
+        85: "21.25rem",
+        100: "25rem",
+      },
+      borderRadius: {
+        "0.5xl": "0.625rem",
+        "2.5xl": "1.25rem",
+      },
+      padding: {
+        7.5: "1.875rem",
+      },
     },
   },
   plugins: [],
