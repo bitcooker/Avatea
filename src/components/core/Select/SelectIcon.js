29ee395dada2cd89804fbd2ca2b2604f049a8044
bitcooker <dreamhighpassionhigh@gmail.com>
2022-06-07 19:23:26 +0200

    Add Radio and Select component

---
+++ src/components/core/Select/SelectIcon.js
@@ -0,0 +1,21 @@
+export default function SelectIcon(props) {
+  return (
+    <div>
+      <svg
+        width="14"
+        height="8"
+        viewBox="0 0 14 8"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1 1L7 7L13 1"
+          stroke="#00204C"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </div>
+  );
+}
