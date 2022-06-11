8387d6b2855751b9125316a59f73c3b10111b2cf
bitcooker <dreamhighpassionhigh@gmail.com>
2022-06-11 11:12:37 +0200

    Add click event to button component

--- src/components/core/Button/Button.js
+++ src/components/core/Button/Button.js
@@ -1,6 +1,9 @@
 export default function Button(props) {
   return (
-    <div className="flex justify-center items-center w-full h-10 bg-indigo-500 text-white rounded-full hover:cursor-pointer hover:bg-indigo-500/80 transition">
+    <div
+      className="flex justify-center items-center w-full h-10 bg-indigo-500 text-white rounded-full hover:cursor-pointer hover:bg-indigo-500/80 transition"
+      onClick={props.handleClick}
+    >
       {props.name}
     </div>
   );
