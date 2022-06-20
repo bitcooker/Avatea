0586294dd0d1e45eae240c59482d98f4397b6c29
bitcooker <dreamhighpassionhigh@gmail.com>
2022-06-20 15:16:17 +0200

    Add Spinner componenet and implement it.

--- src/components/pages/projects/Card/Card.jsx
+++ src/components/pages/projects/Card/Card.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import CardItem from "./CardItem";
 import helper from "../../../../helpers/";
 
+// core components
+import Spinner from "../../../core/Spinner";
+
 export default function Card({ projectProps }) {
   const [projects, setProjects] = useState([]);
 
@@ -13,13 +16,17 @@ export default function Card({ projectProps }) {
   }, []);
 
   return (
-    <div className="grid sm-md:grid-cols-2 xl-2xl:grid-cols-3 2xl-3xl:grid-cols-4 gap-5">
-      {projects.length === 0 ? (
-        <h1>Loading projects...</h1>
+    <div>
+      {projects.length == 0 ? (
+        <div className="flex items-center justify-center w-full h-[85vh]">
+          <Spinner size={5} />
+        </div>
       ) : (
-        projects.map((project) => {
-          return <CardItem key={project.slug} {...project} />;
-        })
+        <div className="grid sm-md:grid-cols-2 xl-2xl:grid-cols-3 2xl-3xl:grid-cols-4 gap-5">
+          {projects.map((project) => {
+            return <CardItem key={project.slug} {...project} />;
+          })}
+        </div>
       )}
     </div>
   );
