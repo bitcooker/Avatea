9294eb1f5f4a827999546c1639c89e50db88a969
bitcooker <dreamhighpassionhigh@gmail.com>
2022-06-20 13:43:02 +0200

    Add ButtonFit and ButtonOutlineFit component and complete vesting overview list page

---
+++ pages/vesting/index.js
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+// core components
+import ButtonFit from "../../src/components/core/Button/ButtonFit";
+
+// vesting components
+import ListItem from "../../src/components/pages/vesting/List/ListItem";
+
+export default function Vesting(props) {
+  return (
+    <div className="flex flex-col h-[85vh] space-y-7.5">
+      <div className="flex flex-row items-center justify-between">
+        <h1 className="text-2xl">Vesting Overview List</h1>
+        <ButtonFit name="Add Vesting" icon="fa-regular fa-plus-large" />
+      </div>
+      <div className="grow space-y-3.75 overflow-hidden overflow-y-scroll scrollbar:!w-1.5">
+        <ListItem projectImage="/coins/ethImage.png" projectName="Ethereum" />
+        <ListItem projectImage="/coins/dotImage.png" projectName="Dot" />
+        <ListItem projectImage="/coins/bnbImage.png" projectName="BNB" />
+        <ListItem projectImage="/coins/avaxImage.png" projectName="Avax" />
+        <ListItem projectImage="/coins/ftmImage.png" projectName="FTM" />
+        <ListItem projectImage="/coins/maticImage.png" projectName="Matic" />
+        <ListItem projectImage="/coins/ethImage.png" projectName="Ethereum" />
+        <ListItem projectImage="/coins/ethImage.png" projectName="Ethereum" />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/core/Button/ButtonFit.js
@@ -0,0 +1,12 @@
+export default function ButtonFit(props) {
+  return (
+    <div
+      className="flex justify-center items-center py-4 px-7.5 bg-indigo-500 text-white rounded-full hover:cursor-pointer hover:bg-indigo-500/80 transition"
+      onClick={props.handleClick}
+    >
+      {props.icon && <i className={props.icon + " mr-2.5"} />}
+      {props.name}
+      {props.children}
+    </div>
+  );
+}
