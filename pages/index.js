4717d28b91f1d3e6e873fc07f033e9a21108a056
bitcooker <dreamhighpassionhigh@gmail.com>
2022-06-20 13:48:28 +0200

    Refactor Home page

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,19 @@
-import helper from '../src/helpers';
+import helper from "../src/helpers";
 import Card from "../src/components/pages/projects/Card/Card";
 
-export default function Home({projects}) {
-
+export default function Home({ projects }) {
   return (
-      <div>
-        <div className="index">
-          <Card projectsProps={projects} />
-        </div>
-      </div>
-  )
+    <div>
+      <Card projectsProps={projects} />
+    </div>
+  );
 }
 
 export async function getServerSideProps(context) {
   const projects = await helper.project.getProjects();
   return {
     props: {
-      projects
-    }
-  }
+      projects,
+    },
+  };
 }
