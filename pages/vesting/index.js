d956a883e9f3468cf5df2380ff4960e12676c7c4
bitcooker <dreamhighpassionhigh@gmail.com>
2022-06-21 17:43:20 +0200

    Adjust responsive for vesting overview list page

--- pages/vesting/index.js
+++ pages/vesting/index.js
@@ -8,12 +8,12 @@ import ListItem from "../../src/components/pages/vesting/List/ListItem";
 
 export default function Vesting(props) {
   return (
-    <div className="flex flex-col h-[85vh] space-y-7.5">
+    <div className="flex flex-col h-[80vh] md-lg:h-[85vh] space-y-7.5">
       <div className="flex flex-row items-center justify-between">
         <h1 className="text-2xl">Vesting Overview List</h1>
         <ButtonFit name="Add Vesting" icon="fa-regular fa-plus-large" />
       </div>
-      <div className="grow space-y-3.75 overflow-hidden overflow-y-auto scrollbar:!w-1.5">
+      <div className="grow space-y-3.75 overflow-hidden overflow-y-auto">
         <ListItem projectImage="/coins/ethImage.png" projectName="Ethereum" />
         <ListItem projectImage="/coins/dotImage.png" projectName="Dot" />
         <ListItem projectImage="/coins/bnbImage.png" projectName="BNB" />
