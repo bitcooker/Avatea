e6706a4afe258975cdd51b93f99ba7f1cf3f329d
bitcooker <dreamhighpassionhigh@gmail.com>
2022-06-22 17:56:48 +0200

    Add one height style(h-110) and add domain for next/image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["avatea-bucket.s3.amazonaws.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
