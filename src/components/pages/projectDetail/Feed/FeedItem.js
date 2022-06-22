3247a865717d15f6fefa18fa1b5956403b650daa
bitcooker <dreamhighpassionhigh@gmail.com>
2022-06-22 17:57:18 +0200

    Add title in FeedItem

--- src/components/pages/projectDetail/Feed/FeedItem.js
+++ src/components/pages/projectDetail/Feed/FeedItem.js
@@ -1,26 +1,35 @@
 import Button from "../../../core/Button/Button";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function FeedItem(props) {
-    const { description, title, image, link} = props.article;
-      return (
-        <div className="flex space-x-5">
-          <div className="flex-none">
-            <img
-              src={image}
-              alt={title}
-              className="w-25 h-25 md-lg:w-38 md-lg:h-36 rounded-2.5xl"
-            />
-          </div>
-          <div className="flex-1">
-            <div className="flex flex-col h-full justify-between">
-              <div>
-                  {description}
-              </div>
+  const { description, title, image, link } = props.article;
+  return (
+    <div className="flex space-x-5">
+      <div className="flex-none">
+        <Image
+          src={image}
+          alt={title}
+          className="md-lg:w-38 md-lg:h-36 rounded-2.5xl"
+          width={155}
+          height={145}
+        />
+      </div>
+      <div className="grow">
+        <div className="flex flex-col h-full justify-between">
+          <span className="text-base font-bold">{title}</span>
+          <div>{description}</div>
 
-                <Button><Link href={link}><>Read more <i className="fa-solid fa-circle-chevron-right pl-2"/></></Link></Button>
-            </div>
-          </div>
+          <Button>
+            <Link href={link}>
+              <>
+                Read more{" "}
+                <i className="fa-solid fa-circle-chevron-right pl-2 mt-0.5" />
+              </>
+            </Link>
+          </Button>
         </div>
-      );
+      </div>
+    </div>
+  );
 }
