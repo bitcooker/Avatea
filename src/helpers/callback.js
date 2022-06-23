6ae5788683535dc2fa7a2ab7ff63ef851059c8f1
Uri <uri@mercor.finance>
2022-06-23 19:25:32 +0300

    fixed transactions.

--- src/helpers/callback.js
+++ src/helpers/callback.js
@@ -56,7 +56,7 @@ const hook = async ({type, data, callback = () => {}}) => {
                 })
                 callback()
                 break;
-            case 'VE':
+            case 'VA':
                 event = data.receipt.events.find(x => x.event === "RewardAdded").args;
                 await axios.post(`${API_URL}Transaction/`, {
                     hash: data.receipt.transactionHash,
@@ -68,7 +68,7 @@ const hook = async ({type, data, callback = () => {}}) => {
                 })
                 callback()
                 break;
-            case 'VA':
+            case 'VE':
                 event = data.receipt.events.find(x => x.event === "Withdrawn").args;
                 await axios.post(`${API_URL}Transaction/`, {
                     hash: data.receipt.transactionHash,
