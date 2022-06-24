b5410f6f522499e7ba124f917e9fddfaa8aa2c3c
Uri <uri@mercor.finance>
2022-06-24 19:55:28 +0300

    adjusted managment page.

--- pages/management/[slug].js
+++ pages/management/[slug].js
@@ -253,7 +253,7 @@ export default function VaultsDetail(props) {
                         id="editPairToken"
                         name="editPairToken"
                         type="number"
-                        placeholder="2324"
+                        placeholder="0"
                     />
                 </div>
                 {/* Max buying amount & Max selling amount */}
@@ -264,7 +264,7 @@ export default function VaultsDetail(props) {
                             id="editMaxBuyingAmount"
                             name="editMaxBuyingAmount"
                             type="number"
-                            placeholder="1234"
+                            placeholder="0"
                         />
                     </div>
                     <div className="w-full space-y-2.5">
@@ -273,9 +273,20 @@ export default function VaultsDetail(props) {
                             id="editMaxSellingAmount"
                             name="editMaxSellingAmount"
                             type="number"
-                            placeholder="1234"
+                            placeholder="0"
+                                                        image={project.image}
                         />
                     </div>
+                </div>
+                <div className="w-full space-y-2.5">
+                    <span className="text-base">Volume per day</span>
+                    <InputWithIcon
+                        id="editPairToken"
+                        name="editPairToken"
+                        type="number"
+                        placeholder="0"
+                        image={project.image}
+                    />
                 </div>
 
 
@@ -414,122 +425,23 @@ export default function VaultsDetail(props) {
                 <div className="card-content grid md-lg:grid-cols-2 gap-3.75">
                     {/* left */}
                     <div className="w-full space-y-3.75">
-                        {/* Project name */}
-                        <div className="w-full space-y-2.5">
-                            <span className="text-base">Project Name</span>
-                            <InputEmpty
-                                id="projectName"
-                                name="projectName"
-                                type="text"
-                                placeholder="Project Name Here"
-                            />
-                        </div>
-                        {/* Pair Token & Base Token */}
-                        <div className="grid md-lg:grid-cols-2 gap-3.75">
-                            <div>
-                                <span className="text-base">Pair Token</span>
-                                <InputWithIcon
-                                    id="pairToken"
-                                    name="pairToken"
-                                    type="number"
-                                    placeholder="2324"
-                                />
-                            </div>
-                            <div>
-                                <span className="text-base">Base Token</span>
-                                <InputWithIcon
-                                    id="baseToken"
-                                    name="baseToken"
-                                    type="number"
-                                    placeholder="2324"
-                                />
-                            </div>
-                        </div>
-                        {/* Description */}
-                        <div className="flex flex-col space-y-3.75">
-                            <h1 className="text-base">Description</h1>
-                            <TextArea
-                                id="description"
-                                name="description"
-                                placeholder="Type here"
-                            />
-                        </div>
-                        {/* Add Social Information */}
-                        <div className="flex flex-col space-y-3.75">
-                            <h1 className="text-base">Add Social Information</h1>
-                            <Select
-                                id="socialName"
-                                name="socialName"
-                                placeholder="Social Name"
-                            />
-                        </div>
-                        {/* Social Items */}
-                        <div className="flex flex-row gap-3.75">
-                            <SocialItem
-                                icon={socialFacebookWithOutBG}
-                                bgColor="bg-indigo-400"
-                            />
-                            <SocialItem
-                                icon={socialTwitterWithOutBG}
-                                bgColor="bg-sky-400"
-                            />
-                            <SocialItem
-                                icon={socialLinkedWithOutBG}
-                                bgColor="bg-indigo-400"
-                            />
-                            <SocialItem
-                                icon={socialTelegramWithOutBG}
-                                bgColor="bg-sky-400"
-                            />
-                        </div>
-                    </div>
-                    {/* right */}
-                    <div className="flex flex-col w-full space-y-3.75">
-                        {/* Project name */}
-                        <div className="w-full space-y-2.5">
-                            <span className="text-base">Website</span>
-                            <InputEmpty
-                                id="projectName"
-                                name="projectName"
-                                type="text"
-                                placeholder="Enter Your Website"
-                            />
-                        </div>
-                        {/* Whitepaper & Audit */}
-                        <div className="grid md-lg:grid-cols-2 gap-3.75">
-                            <div>
-                                <span className="text-base">Whitepaper</span>
-                                <InputEmpty
-                                    id="whitepager"
-                                    name="whitepager"
-                                    type="text"
-                                    placeholder="Whitepager"
-                                />
-                            </div>
-                            <div>
-                                <span className="text-base">Audit</span>
-                                <InputEmpty
-                                    id="audit"
-                                    name="audit"
-                                    type="text"
-                                    placeholder="Audit"
-                                />
-                            </div>
-                        </div>
-                        {/* Inside Banner */}
-                        <div className="grow w-full space-y-3.75">
-                            <span className="text-base">Banner</span>
-                            <div className="w-full h-full">
-                                <img
-                                    src="/projects/default_banner.png"
-                                    className="w-full h-[85%] rounded-0.5xl"
-                                />
-                            </div>
-                        </div>
                         {/* Edit Button */}
                         <Button
                             name="Edit Information"
                             handleClick={() => setOpenEditProject(true)}
+                        />
+                        <Button
+                            name="Edit Articles"
+                        />
+                    </div>
+                    {/* right */}
+                    <div className="flex flex-col w-full space-y-3.75">
+                        {/* Edit Button */}
+                        <Button
+                            name="Create Vesting schedules"
+                        />
+                        <Button
+                            name="Stake for participants"
                         />
                     </div>
                 </div>
