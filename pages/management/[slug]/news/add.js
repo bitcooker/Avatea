f0a668d97ff16f56646de06e9df59aa471a07940
bitcooker <dreamhighpassionhigh@gmail.com>
2022-06-28 20:12:46 +0200

    Build Tooltip component and update add news page with test tooltip

--- pages/management/[slug]/news/add.js
+++ pages/management/[slug]/news/add.js
@@ -6,6 +6,7 @@ import ButtonFit from "../../../../src/components/core/Button/ButtonFit";
 import ButtonOutlineFit from "../../../../src/components/core/Button/ButtonOutlineFit";
 import InputEmpty from "../../../../src/components/core/Input/InputEmpty";
 import RichEditor from "../../../../src/components/core/RichEditor/RichEditor";
+import Tooltip from "../../../../src/components/core/Tooltip/Tooltip";
 
 export default function News(props) {
   const router = useRouter();
@@ -13,11 +14,17 @@ export default function News(props) {
   return (
     <div className="flex flex-col min-h-[80vh] md-lg:min-h-[85vh] space-y-7.5">
       <div className="flex flex-row items-center justify-between">
-        <h1 className="text-2xl">News Add</h1>
+        <h1 className="text-2xl">News Add </h1>
       </div>
       <div className="flex flex-col p-7.5 bg-white gap-5 rounded-2xl overflow-hidden hover:scrollbar-thin hover:scrollbar-thumb-gray-200">
         <div className="flex flex-col space-y-3.75">
-          <h1 className="text-xl">News Title</h1>
+          <div className="flex fle-row">
+            <h1 className="text-xl mr-2">News Title</h1>
+              <span className="relative flex flex-col items-center justify-center group">
+                <i className="fa-regular fa-circle-info text-sky-500 text-base mt-0.5" />
+                <Tooltip title="This is test tooltip"/>
+              </span>
+          </div>
           <InputEmpty id="title" name="title" placeholder="Enter News title"/>
         </div>
         <div className="grow">
