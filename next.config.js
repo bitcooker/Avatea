406956308c23749aeedca8d9197b78a3d59aa611
bitcooker <dreamhighpassionhigh@gmail.com>
2022-07-07 11:45:22 +0200

    Add new domain for next/image to next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["avatea-bucket.s3.amazonaws.com"],
+    domains: ["avatea-bucket.s3.amazonaws.com", "s2.coinmarketcap.com"],
   },
 };
 
