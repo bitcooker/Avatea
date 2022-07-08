94a188d945674477087be9a0ec4aad2596699cd9
bitcooker <dreamhighpassionhigh@gmail.com>
2022-07-08 19:15:43 +0200

    Update setModalOption funtion and add min height to main div

--- src/components/Header.js
+++ src/components/Header.js
@@ -97,7 +97,7 @@ export default function Header({ menu, setMenu, title }) {
               </button>
             </div>
           )}
-          <WalletConnect open={modalOpen} handleClose={() => setModalOpen(!modalOpen)}/>
+          <WalletConnect open={modalOpen} handleClose={() => setModalOpen(false)}/>
         </div>
       </div>
     </header>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -41,7 +41,7 @@ export default function Layout({ children }) {
       >
         <Sidebar menu={menu} setMenu={setMenu} setTitle={setTitle} />
         <main>
-          <div className="pb-5 px-[15px] lg-xl:px-5 lg-xl:pl-[220px] xl-2xl:pr-5 xl-2xl:pl-[295px]">
+          <div className="min-h-[100vh] pb-5 px-[15px] lg-xl:px-5 lg-xl:pl-[220px] xl-2xl:pr-5 xl-2xl:pl-[295px]">
             <Header menu={menu} setMenu={setMenu} title={title} />
             {children}
             <ToastContainer
