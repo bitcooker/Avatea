7f227b22552aab87350cd5fe3b4e529cdbb6e2d4
bitcooker <dreamhighpassionhigh@gmail.com>
2022-07-11 13:57:12 +0200

    Remove search box and restyle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
-import Image from "next/image";
-import { useWallet } from "use-wallet";
-import helpers from "../helpers";
 import {useCallback, useEffect, useState} from "react";
-import hashicon from "hashicon";
-import networks from "./../network/network.json";
-import SwitchNetwork from "../components/core/SwitchNetwork";
-import {ethers} from "ethers";
 import useLocalStorage from "use-local-storage";
+import { useWallet } from "use-wallet";
+import Image from "next/image";
+import {ethers} from "ethers";
+import hashicon from "hashicon";
+
+import helpers from "../helpers";
+import networks from "./../network/network.json";
+
+// core components
 import WalletConnect from "./core/WalletConnect";
+import SwitchNetwork from "./core/SwitchNetwork";
 
 export default function Header({ menu, setMenu, title }) {
   const wallet = useWallet();
@@ -48,28 +51,16 @@ export default function Header({ menu, setMenu, title }) {
         </div>
         <h1 className="hidden lg-xl:block lg-xl:text-2xl">{title}</h1>
         <div className="flex items-center">
-          <form className="absolute bottom-5 left-0 w-full md-lg:static md-lg:w-75 md-lg:mr-6.25">
-            <div className="relative w-full">
-              <input
-                className="w-full text-base text-black bg-white shadow-[0_6px_12px_rgba(155,0,231,0.1)] rounded-full py-4 px-12"
-                type="text"
-                placeholder="Search"
-              />
-              <div className="absolute left-5 top-5 hover:cursor-pointer">
-                <i className="fa-regular fa-magnifying-glass text-lg"></i>
-              </div>
-            </div>
-          </form>
-          <i className="relative fa-light fa-bell text-2xl mr-6.25 hover:cursor-pointer after:content-[''] after:w-2 after:h-2 after:absolute after:right-0 after:rounded-full after:bg-rose-500 after:hover:ring-2 after:hover:ring-rose-500/30 transition" />
+          <i className="relative fa-light fa-bell text-2xl mr-6.25 hover:cursor-pointer after:content-[''] after:w-2 after:h-2 after:absolute after:right-0 after:rounded-full after:bg-rose-500 after:hover:ring-2 after:hover:ring-rose-500/30 after:transition" />
           <div className="mr-6.25">
             <SwitchNetwork />
           </div>
           {wallet.status === "connected" ? (
             <div className="flex flex-row items-center bg-white p-1 rounded-4xl">
-              <span className="hidden sm:block text-base px-2 ">
-                {Number(ethers.utils.formatEther(wallet.balance)).toFixed(4)} <img src={networks[0].icon} className={'inline w-[20px] h-[20px]'}/>
-
-              </span>
+              <div className="hidden sm:flex sm:items-center px-2">
+                <span className="mr-1">{Number(ethers.utils.formatEther(wallet.balance)).toFixed(4)}</span>
+                <Image src={networks[0].icon} alt="netImage" width={20} height={20}/>
+              </div>
               <button
                 className="flex justify-center box-border items-center px-7.5 py-3 bg-indigo-500 text-white rounded-4xl hover:cursor-pointer hover:bg-indigo-500/80 transition"
                 onClick={() => wallet.reset()}
