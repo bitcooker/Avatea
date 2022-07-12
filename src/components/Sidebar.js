77fa52e5675a3c8db3b4b334e081252646243eb1
bitcooker <dreamhighpassionhigh@gmail.com>
2022-07-12 12:27:56 +0200

    Add framer motion to Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,7 @@ import React, {useCallback, useEffect, useState} from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { motion } from "framer-motion";
 import helper from "../helpers/";
 import { useWallet } from "use-wallet";
 import { ethers } from "ethers";
@@ -34,38 +35,45 @@ const menus = [
   },
 ];
 
+const variants = {
+    open: { opacity: 1, zIndex: 50 },
+    close: { opacity: 0, transitionEnd: { zIndex: 0 }}
+}
+
 export default function Sidebar({ menu, setMenu, setTitle }) {
 
   return (
-    <div
-      className={
-        "fixed w-[100vw] h-[100vh] bg-black/30 top-0 left-0 z-50 lg-xl:rounded-r-2.5xl transition duration-300 bg-white lg-xl:translate-x-0 lg-xl:w-50 xl-2xl:w-66.25 " +
-        (menu ? "translate-x-0" : "-translate-x-full")
-      }
-      onClick={() => setMenu(false)}
-    >
-      <div className="px-3.75 max-w-66.25 bg-white rounded-r-2.5xl h-[100vh]">
-        <div className="flex items-center justify-start px-3.75">
-          <Image src={"/logo.svg"} alt="logo" width={126} height="100%" />
+    <div className="relative h-[100%]">
+        <motion.div initial={{ opacity: 0 }} variants={variants} animate={menu ? "open" : "close"} className="overlay fixed w-[100vw] h-[100vh] bg-black/30" />
+        <div
+            className={
+                "fixed w-[100vw] h-[100vh] top-0 left-0 z-50 lg-xl:rounded-r-2.5xl transition duration-300 lg-xl:translate-x-0 lg-xl:w-50 xl-2xl:w-66.25 " +
+                (menu ? "translate-x-0" : "-translate-x-full")
+            }
+            onClick={() => setMenu(false)}
+            >
+            <div className="px-3.75 max-w-66.25 bg-white rounded-r-2.5xl h-[100vh] z-1500">
+                <div className="flex items-center justify-start px-3.75">
+                    <Image src={"/logo.svg"} alt="logo" width={126} height="100%" />
+                </div>
+                <nav
+                    className="relative flex flex-col justify-between"
+                    style={{ height: "calc(100% - 105px)" }}
+                >
+                    <div className="flex flex-col">
+                        {menus.map((menu, index) => (
+                            <MenuItem
+                                href={menu.href}
+                                label={menu.label}
+                                icon={menu.icon}
+                                setTitle={setTitle}
+                                key={index}
+                            />
+                        ))}
+                    </div>
+                </nav>
+            </div>
         </div>
-        <nav
-          className="relative flex flex-col justify-between"
-          style={{ height: "calc(100% - 105px)" }}
-        >
-          <div className="flex flex-col">
-            {menus.map((menu, index) => (
-              <MenuItem
-                href={menu.href}
-                label={menu.label}
-                icon={menu.icon}
-                setTitle={setTitle}
-                key={index}
-              />
-            ))}
-          </div>
-
-        </nav>
-      </div>
     </div>
   );
 }
