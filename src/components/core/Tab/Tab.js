80ba32f9de3946cd189dfa6e3e0dcfbdd6d04da2
bitcooker <dreamhighpassionhigh@gmail.com>
2022-07-12 11:01:14 +0200

    Hide scrollbar while scrolling

--- src/components/core/Tab/Tab.js
+++ src/components/core/Tab/Tab.js
@@ -5,7 +5,7 @@ import TabItem from "./TabItem";
 export default function Tab(props) {
   return (
     <span 
-        className="flex flex-row items-center rounded-[24px] bg-white inline-flex p-2 space-x-2 overflow-hidden hover:scrollbar-thin hover:scrollbar-thumb-gray-200"
+        className="flex flex-row items-center rounded-[24px] bg-white inline-flex p-2 space-x-2 overflow-scroll scrollbar-none"
     >
       {props.items.map((item, index) => (
         <TabItem
