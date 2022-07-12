713be6e2b35210778f8a3d3450191e9914f09844
bitcooker <dreamhighpassionhigh@gmail.com>
2022-07-12 09:51:20 +0200

    Restyle Tab menu

--- src/components/core/Tab/TabItem.js
+++ src/components/core/Tab/TabItem.js
@@ -3,8 +3,8 @@ export default function TabItem(props) {
     <div
       className={
         props.selected
-          ? "rounded-[20px] px-5 py-1 bg-indigo-500 text-white hover:cursor-pointer transition"
-          : "rounded-[20px] px-5 py-1 bg-gray-100 hover:cursor-pointer transition"
+          ? "rounded-[20px] whitespace-nowrap px-5 py-1 bg-indigo-500 text-white hover:cursor-pointer transition"
+          : "rounded-[20px] whitespace-nowrap px-5 py-1 bg-gray-100 hover:cursor-pointer transition"
       }
       onClick={() => props.handleSetTab(props.value)}
     >
