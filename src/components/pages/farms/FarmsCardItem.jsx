dbc69d2b064ad0ad6597ae633457b92264f445c6
bitcooker <dreamhighpassionhigh@gmail.com>
2022-07-15 17:45:55 +0200

    Fix FarmsCardItem style

--- src/components/pages/farms/FarmsCardItem.jsx
+++ src/components/pages/farms/FarmsCardItem.jsx
@@ -12,7 +12,7 @@ export default function FarmsCardItem(props) {
                 <div className="flex justify-between items-center">
                     <Image src={props.image} alt="farmImage" width={64} height={64} objectFit="none" />
                     <div className="flex flex-col">
-                        <h1 className="text-2xl font-bold">{props.title}</h1>
+                        <h1 className="text-2xl font-semibold">{props.title}</h1>
                         <div className="flex gap-2 justify-end">
                             <div className="flex items-center px-2 py-1 gap-2 border-2 border-green-500 rounded-full">
                                 <i className="fa-solid fa-badge-check text-green-500 text-lg" />
@@ -25,36 +25,31 @@ export default function FarmsCardItem(props) {
                     </div>
                 </div>
 
-                <div className="flex p-2 flex-col text-base text-black-500">
+                <div className="flex p-2 flex-col text-black-500">
                     <div className="flex justify-between">
                         <span>APR:</span>
-                        <span className="font-bold">{props.apr}%</span>
+                        <span className="font-semibold">{props.apr}%</span>
                     </div>
                     <div className="flex justify-between">
                         <span>Stake:</span>
-                        <span className="font-bold">{props.stake}</span>
+                        <span className="font-semibold">{props.stake}</span>
                     </div>
                     <div className="flex justify-between">
                         <span>Earn:</span>
-                        <span className="font-bold">{props.earn}</span>
+                        <span className="font-semibold">{props.earn}</span>
                     </div>
                 </div>
 
-                <div className="flex px-2 justify-between text-2xl">
-                    <span>Deposit Fee:</span>
-                    <span className="font-bold">{props.depositFee}%</span>
-                </div>
-
                 <div className="flex flex-col px-2 gap-1">
-                    <label><span className="font-bold">{props.earn}</span> EARNED</label>
+                    <label className="text-indigo-500"><span className="font-semibold">{props.earn}</span> EARNED</label>
                     <div className="grid grid-cols-2 gap-2">
                         <InputEmpty id="mercor-earned" name="earned" type="number" value="0" readOnly />
-                        <ButtonFit name="Harvest" />
+                        <ButtonFit name="Harvest" disabled />
                     </div>
                 </div>
 
-                <div className="flex flex-col px-2 gap-1">
-                    <label><span className="font-bold">{props.title}</span> STAKED</label>
+                <div className="flex flex-col px-2 gap-1 text-indigo-500">
+                    <label><span className="font-semibold">{props.title}</span> STAKED</label>
                     <ButtonFit name="Unlock Wallet" />
                 </div>
             </div>
