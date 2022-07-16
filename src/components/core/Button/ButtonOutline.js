ae930c8736ac701720ae149ae8ae426766fdc862
bitcooker <dreamhighpassionhigh@gmail.com>
2022-07-16 14:26:51 +0200

    Remvoe fixed height and add py in Button components

--- src/components/core/Button/ButtonOutline.js
+++ src/components/core/Button/ButtonOutline.js
@@ -3,7 +3,7 @@ import { Loading } from "../../SVG";
 export default function ButtonOutline(props) {
   return (
     <button
-      className="flex justify-center items-center w-full h-10 border border-indigo-500 text-indigo-500 rounded-full hover:cursor-pointer hover:bg-indigo-500 hover:text-white transition"
+      className="flex justify-center items-center w-full py-2.5 border border-indigo-500 text-indigo-500 rounded-full hover:cursor-pointer hover:bg-indigo-500 hover:text-white transition"
       onClick={props.handleClick}
       disabled={props.disabled ? props.disabled : false}
     >
