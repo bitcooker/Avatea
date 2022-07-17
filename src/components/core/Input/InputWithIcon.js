2d2b966d84cbe8f45a93fab3492e6162bb3badbc
bitcooker <dreamhighpassionhigh@gmail.com>
2022-07-17 11:38:55 +0200

    Restyle InputWithIcon component  and add new url for next/image

--- src/components/core/Input/InputWithIcon.js
+++ src/components/core/Input/InputWithIcon.js
@@ -1,20 +1,19 @@
 import * as React from "react";
-import InputSubmit from "./InputSubmit";
+import Image from "next/image";
 
 export default function Input(props) {
-  const [value, setValue] = React.useState(props.value);
-
-
   return (
-    <div className="flex shadow-sm h-12.5 block w-full bg-gray-100 rounded-0.5xl pl-5 pr-3.75 py-2.5">
-      <img src={props.image ? props.image : "/avatea-token.png"} className="w-6 h-6 mr-3.75" />
+    <div className="flex items-center shadow-sm h-12.5 w-full bg-gray-100 rounded-0.5xl pl-5 pr-3.75 py-2.5 gap-2">
+      <div className="w-6 h-6">
+        <Image src={props.image ? props.image : "/avatea-token.png"} alt="tokenImage" className="mr-3.75" layout="fixed" width={24} height={24}/>
+      </div>
       <input
         id={props.id}
         name={props.name}
         type={props.type}
         value={props.value}
         onChange={e => props.setValue(e.target.value)}
-        className="block w-full bg-gray-100"
+        className="w-full bg-gray-100"
         placeholder={props.placeholder}
       />
     </div>
