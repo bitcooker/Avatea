01b658c8e7a148abb9e382eba02c9ba25cc6f5ad
bitcooker <dreamhighpassionhigh@gmail.com>
2022-07-18 14:50:15 +0200

    Add opactiy to setting & chain Image bg

--- src/components/pages/projectDetail/Banner/Banner.js
+++ src/components/pages/projectDetail/Banner/Banner.js
@@ -3,7 +3,7 @@ import * as React from "react";
 import Image from "next/image";
 import BannerContent from "./BannerContent";
 
-export default function Preview(props) {
+export default function Banner(props) {
   return (
     <div
       className="relative w-full h-85 md-lg:h-100 rounded-2.5xl"
@@ -13,6 +13,9 @@ export default function Preview(props) {
         backgroundPosition: "center",
       }}
     >
+      <div className="absolute flex items-center justify-center top-2 right-2 w-16 h-16 bg-white/20 rounded-0.5xl">
+        <Image src="https://avatea-bucket.s3.amazonaws.com/media/images/Binance-coin-bnb-logo_s1xsqhX.png" alt="chainImage" width={48} height={48}/>
+      </div>
       <BannerContent {...props}/>
     </div>
   );
