ac0d817850a0c6683abe61556bed0ec445a90df7
A <albertsamseer@MacBook-Air-van-Albert.local>
2022-07-19 09:54:57 +0200

    Remove pulse

--- src/components/Header.js
+++ src/components/Header.js
@@ -59,7 +59,7 @@ export default function Header({ menu, setMenu, title }) {
         <div className="flex items-center">
             <div className={'relative mr-5 '}>
                 <span className="flex h-5 w-5 absolute -top-2 -right-2.5 z-10">
-                  <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
+                  {/*<span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>*/}
                   <span className="relative inline-flex rounded-full h-4 w-4 bg-red-500"></span>
                 </span>
                 <span className="flex absolute -top-[0.45rem] -right-[0.1rem] text-white text-xs z-20">
