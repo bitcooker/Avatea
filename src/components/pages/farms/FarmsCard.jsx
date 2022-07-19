14590a533142dca9da7b517282574eaf5d900ca5
bitcooker <dreamhighpassionhigh@gmail.com>
2022-07-19 14:41:03 +0200

    Add details collapse to farms card

--- src/components/pages/farms/FarmsCard.jsx
+++ src/components/pages/farms/FarmsCard.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 export default function FarmsCard(props) {
     return (
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 items-baseline gap-5">
             {props.children}
         </div>
     )
--- src/components/pages/farms/FarmsCardItem.jsx
+++ src/components/pages/farms/FarmsCardItem.jsx
@@ -6,6 +6,8 @@ import InputEmpty from "../../../components/core/Input/InputEmpty";
 import ButtonFit from "../../../components/core/Button/ButtonFit";
 
 export default function FarmsCardItem(props) {
+    const [openDetail, setOpenDetail] = React.useState(false);
+
     return (
         <div className="flex flex-col p-6 bg-white rounded-2.5xl gap-5 divide-y">
             <div className="flex flex-col gap-4">
@@ -54,8 +56,29 @@ export default function FarmsCardItem(props) {
                 </div>
             </div>
 
-            <div className="flex justify-center items-center gap-2 pt-5 text-indigo-500 hover:text-indigo-500/80 hover:cursor-pointer transition">
-                Details <i className="fa-solid fa-arrow-down" />
+            {/* details collapse */}
+            <div className="flex flex-col gap-2 pt-5 ">
+                <div className="flex justify-center items-center text-indigo-500 hover:text-indigo-500/80 hover:cursor-pointer gap-2 transition" onClick={() => setOpenDetail((openDetail) => !openDetail)}>
+                    Details {!openDetail ? <i className="fa-solid fa-arrow-down" /> : <i className="fa-solid fa-arrow-up" />}
+                </div>
+                <div className={`flex flex-col gap-2 overflow-hidden transition-all ${openDetail ? 'max-h-[110px]' : 'max-h-0'}`}>
+                    <div className="flex justify-between">
+                        <span>Total Liquidity:</span>
+                        <span>$135,785,231</span>
+                    </div>
+                    <a className="flex justify-between text-indigo-500 hover:text-indigo-500/80 hover:cursor-pointer transition">
+                        <span>Get {props.title}</span>
+                        <span><i className="fa-solid fa-arrow-up-right-from-square" /></span>
+                    </a>
+                    <a className="flex justify-between text-indigo-500 hover:text-indigo-500/80 hover:cursor-pointer transition">
+                        <span>View Contract</span>
+                        <span><i className="fa-solid fa-arrow-up-right-from-square" /></span>
+                    </a>
+                    <a className="flex justify-between text-indigo-500 hover:text-indigo-500/80 hover:cursor-pointer transition">
+                        <span>See Pair Info</span>
+                        <span><i className="fa-solid fa-arrow-up-right-from-square" /></span>
+                    </a>
+                </div>
             </div>
         </div>
     )
