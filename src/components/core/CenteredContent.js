dfcb725386f738e55319bae224f7c31ba694e7f5
bitcooker <dreamhighpassionhigh@gmail.com>
2022-07-19 14:55:21 +0200

    Restyle ConnectYourWallet

--- src/components/core/CenteredContent.js
+++ src/components/core/CenteredContent.js
@@ -2,8 +2,8 @@ import Card from "../pages/projectDetail/Card/Card";
 
 export default function CenteredContent(props) {
     return (
-        <Card>
-            <div className="grid grid-cols-1 gap-4 content-center block text-center place-content-center" >
+        <Card className="h-[85vh] flex items-center justify-center">
+            <div className="grid grid-cols-1 gap-4 content-center text-center place-content-center" >
                 {props.children}
             </div>
         </Card>
--- src/components/core/ConnectYourWallet.js
+++ src/components/core/ConnectYourWallet.js
@@ -6,7 +6,7 @@ export default function ConnectYourWallet(){
         <CenteredContent>
             <span className={'text-2xl'}>Please connect your wallet</span>
             <div className={'w-[50%] mx-auto'}>
-                <Image src={'/metamask.gif'} layout={'responsive'} height={200} width={200} objectFit={'contain'}/>
+                <Image src={'/metamask.gif'} alt="" layout={'responsive'} height={200} width={200} objectFit={'contain'}/>
             </div>
             {/*<Button handleClick={() => setTab(0)}>Return to project</Button>*/}
         </CenteredContent>
