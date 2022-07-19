a0ab7281d8afcd83cc764ef06f387ff33977aa46
bitcooker <dreamhighpassionhigh@gmail.com>
2022-07-19 14:39:10 +0200

    Fix style issue in ButtonFit(disabled)

--- src/components/core/Button/ButtonFit.js
+++ src/components/core/Button/ButtonFit.js
@@ -3,7 +3,7 @@ import { Loading } from "../../SVG";
 export default function ButtonFit(props) {
   return (
     <div
-      className={`flex justify-center items-center py-4 px-7.5 bg-indigo-500 text-white rounded-full ${props.disabled ? "hover:cursor-not-allowed bg-gray-100 text-gray-500" : "hover:cursor-pointer hover:bg-indigo-500/80"} transition`}
+      className={`flex justify-center items-center py-4 px-7.5 bg-indigo-500 text-white rounded-full ${props.disabled ? "hover:cursor-not-allowed bg-gray-100 !text-gray-500" : "hover:cursor-pointer hover:bg-indigo-500/80"} transition`}
       onClick={props.handleClick}
       disabled={props.disabled ? props.disabled : false}
     >
