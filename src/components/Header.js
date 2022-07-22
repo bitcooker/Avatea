b9d386e707ba4c3e0baeb12d099c1aeb4dfa51b1
A <albertsamseer@MacBook-Air-van-Albert.local>
2022-07-22 13:42:32 +0200

    Add link

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,9 +12,11 @@ import networks from "./../network/network.json";
 // core components
 import WalletConnect from "./core/WalletConnect";
 import SwitchNetwork from "./core/SwitchNetwork";
+import {useRouter} from "next/router";
 
 export default function Header({ menu, setMenu, title }) {
   const wallet = useWallet();
+  const router = useRouter();
   const [unreadMessages, setUnreadMessages] = useState(0);
   const [isRegistered,setIsRegistered] = useLocalStorage('isRegistered', false);
   const [modalOpen, setModalOpen] = useState(false);
@@ -62,7 +64,7 @@ export default function Header({ menu, setMenu, title }) {
         {/* title */}
         <h1 className="hidden lg-xl:block lg-xl:text-2xl">{title}</h1>
         <div className="flex items-center">
-            <div className={'hidden md-lg:flex relative mr-5 '}>
+            <div className={'hidden md-lg:flex relative mr-5 '} onClick={() => router.push('/inbox')}>
                 <span className="flex h-5 w-5 absolute -top-2 -right-2.5 z-10">
                   {/*<span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>*/}
                   <span className="relative inline-flex rounded-full h-4 w-4 bg-red-500"></span>
