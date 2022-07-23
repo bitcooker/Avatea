c909d940f5b5c1ada926616887bbd27da412446f
bitcooker <dreamhighpassionhigh@gmail.com>
2022-07-23 14:05:54 +0200

    Fix richEditor toolbar issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     domains: ["avatea-bucket.s3.amazonaws.com", "s2.coinmarketcap.com", "www.blocknative.com", "app.uniswap.org", "rinkeby.etherscan.io","127.0.0.1"],
   },
