1e00e71342975c55b0e7eb130b44725ecb8b84f6
A <albertsamseer@MacBook-Air-van-Albert.local>
2022-07-24 09:39:43 +0200

    Add nprogress loader

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,11 @@ import "../styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
 import "react-multi-carousel/lib/styles.css";
 import Layout from "../src/components/Layout";
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start()); Router.events.on('routeChangeComplete', () => NProgress.done()); Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
