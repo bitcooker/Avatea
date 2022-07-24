726c834fd1540d4e71b05f98d5719152b5bce9ac
A <albertsamseer@MacBook-Air-van-Albert.local>
2022-07-24 21:04:14 +0200

    Hide envelope if messages = 0

--- src/components/Header.js
+++ src/components/Header.js
@@ -68,7 +68,7 @@ export default function Header({ menu, setMenu }) {
         {/* title */}
         <h1 className="hidden lg-xl:block lg-xl:text-2xl">{title}</h1>
         <div className="flex items-center">
-            <div className={'hidden md-lg:flex relative mr-5 '} onClick={() => router.push('/inbox')}>
+            <div className={`relative mr-5 ${unreadMessages > 0 ? "md-lg:flex " : "hidden"}`} onClick={() => router.push('/inbox')}>
                 <span className="flex h-5 w-5 absolute -top-2 -right-2.5 z-10">
                   {/*<span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>*/}
                   <span className="relative inline-flex rounded-full h-4 w-4 bg-red-500"></span>
