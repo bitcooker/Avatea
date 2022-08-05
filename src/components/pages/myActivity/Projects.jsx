b1b24b346a72db64288a763ec12024a53fe593cd
A <albertsamseer@MacBook-Air-van-Albert.local>
2022-08-05 10:26:01 +0200

    Remove heading in activity and color nprogress

--- src/components/pages/myActivity/Projects.jsx
+++ src/components/pages/myActivity/Projects.jsx
@@ -15,7 +15,6 @@ export default function Projects({projects, loaded}) {
                     </div>
                 ) : (projects.length === 0 && loaded === true ? "No active projects" :
                         <div className="flex flex-col space-y-5">
-                            <h2 className={'text-2xl col-span-full'}>Active in these projects</h2>
                             <div className="w-full flex snap-x snap-mandatory overflow-x-auto scrollbar-none sm-md:grid sm-md:grid-cols-2 xl-2xl:grid-cols-2 gap-5 py-5">
                                 {projects.map((project, index) =>
                                     <div className="w-full snap-center shrink-0" key={index}>
