20b330356a7c49dd1e8ecb982586ca5e547c7fb9
A <albertsamseer@MacBook-Air-van-Albert.local>
2022-08-08 10:47:52 +0200

    change link

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
         />
-              <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@500&display=swap"
+              <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:500&display=swap"
                     rel="stylesheet"/>
           <link rel="shortcut icon" href="/favicon.png" />
       </Head>
