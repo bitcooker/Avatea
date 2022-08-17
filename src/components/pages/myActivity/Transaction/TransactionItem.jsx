58d3ff6aaf1c9321ae5e60d6a8c80131b15651bc
A <albertsamseer@MacBook-Air-van-Albert.local>
2022-08-17 15:11:57 +0200

    Improve transaction

--- src/components/pages/myActivity/Transaction/TransactionItem.jsx
+++ src/components/pages/myActivity/Transaction/TransactionItem.jsx
@@ -10,7 +10,7 @@ const TRANSACTION_TYPES = {
     'VR': 'Vault Rewards', // Done
     'VA': 'Vault Reward Adding', // Done
     'LMW': 'Liquidity Maker Withdrawal', // Done
-    'LML': 'Liquidity Maker Liquidity Adding', // Done
+    'LMD': 'Liquidity Maker LP Deposit',
     'MMBD': 'Market Making Base Deposit', // Done
     'MMPD': 'Market Making Paired Deposit', // Done
     'MMVD': 'Market Making Vesting Deposit', // Done
@@ -21,19 +21,21 @@ const TRANSACTION_TYPES = {
     'MMAS': 'Market Making Allow Selling', // Done
     'MMBR': 'Market Making Base Staking Ratio', // Done
     'MMBB': 'Market Making Base Batch',
+    'MMVA': 'Market Making Vesting Automated Release',
     'AR': 'Avatea Rewards',
     'VD': 'Vault Deposit',
     'VE': 'Vault Exit',
-    'LMD': 'Liquidity Maker Deposit',
     'MMSC': 'Market Making Settings Change',
     'MMRT': 'Market Making Revoke Tokens',
     'MMAR': 'Market Making Allow Releasing',
     'MMPR': 'Market Making Paired Staking Ratio',
-    'LMR': 'Liquidity Maker Rewards',
-    'LMC': 'Liquidity Maker Compound',
+    'LMR': 'Liquidity Maker Rewards Claiming',
+    'LMC': 'Liquidity Maker Liquidity Compound',
     'LME': 'Liquidity Maker Exit',
     'LMA': 'Liquidity Maker Reward Adding',
-
+    'LMB': 'Liquidity Maker Base Deposit',
+    'LMP': 'Liquidity Maker Paired Deposit',
+    'LML': 'Liquidity Maker Liquidity Reward Adding'
 }
 
 const CheckMark = () => {
@@ -72,8 +74,11 @@ export default function TransactionItem(props) {
     return (
         <div className="grid grid-cols-12 items-center w-full p-2">
             <TableCol className="truncate col-span-7 font-medium text-base tracking-[.0125rem]">
-                <div>
+                <div className={'mb-3'}>
+                    <div className={`inline-block px-3 py-2 rounded text-white border-2 border-opacity-50 border-gray-300 text-black hover:bg-gray-300 delay-150 transition-all hover:border-opacity-100 text-center`}>
+{/*
                     <div className={`inline-block px-2 py-2 rounded-full text-white`} style={{backgroundColor: stc(props.type)}}>
+*/}
                         <div className="hidden sm:flex items-center gap-2 text-sm leading-none">
                             {props.image ?
                                 <Image src={props.image} alt="" width={16} height={16}/>
@@ -84,7 +89,7 @@ export default function TransactionItem(props) {
                         <p className="sm:hidden text-sm leading-none">{props.type}</p>
                     </div>
                 </div>
-                <small>{moment(props.timestamp).format('llll')}</small>
+                <small><i className="fa-solid fa-timer mr-1"></i> {moment(props.timestamp).format('llll')}</small>
             </TableCol>
             <TableCol className="col-span-4 font-medium text-base tracking-[.0125rem] text-white">
                 <div className={`text-slate-600 text-sm`}>
@@ -94,9 +99,9 @@ export default function TransactionItem(props) {
                 </div>
             </TableCol>
             <TableCol className="col-span-1 flex items-center justify-center">
-                <a target={'_blank'} href={`https://rinkeby.etherscan.io/tx/${props.hash}`} rel={'noreferrer'}>
-                    <div className={`flex items-center justify-center w-6 h-6 pt-[1.5px] rounded-md border border-slate-600/50 hover:border-slate-600`}>
-                        <i className={`fa-solid fa-eye fa-xs text-slate-600`}/>
+                <a target={'_blank'} href={`https://rinkeby.etherscan.io/tx/${props.hash}`} rel={'noreferrer'} className={'hover:cursor-pointer'}>
+                    <div className={`hover:cursor-pointer flex items-center justify-center w-6 h-6 pt-[1.5px] rounded-md border border-slate-600/50 hover:border-slate-600`}>
+                        <i className={`cursor-pointer fa-solid fa-eye fa-xs text-slate-600`}/>
                     </div>
                 </a>
             </TableCol>
