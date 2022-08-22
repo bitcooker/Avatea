d90efe4afad6a14ad920fea66b40181baf83a284
bitcooker <dreamhighpassionhigh@gmail.com>
2022-08-22 15:00:38 +0200

    fix SkeletonMarketMaking

--- src/components/pages/projects/Skeleton/SkeletonMarketMaking.js
+++ src/components/pages/projects/Skeleton/SkeletonMarketMaking.js
@@ -28,24 +28,6 @@ export default function SkeletonMarketMaking(props) {
                         </div>
                     </div>
                 </div>
-                <div className="card-content pt-4.5 space-y-3.75">
-                    <div className="flex justify-between">
-                        <div className="rounded w-1/6 h-5 bg-slate-200" />
-                        <div className="flex flex-row w-1/6 gap-x-1">
-                            <div className="rounded w-1/2 h-5 bg-slate-200" />
-                            <div className="rounded w-1/2 h-5 bg-slate-200" />
-                        </div>
-                    </div>
-                    <div className="rounded w-full h-12.5 bg-slate-200"></div>
-                    <div className="flex justify-between">
-                        <div className="rounded w-1/6 h-5 bg-slate-200" />
-                        <div className="flex flex-row w-1/6 gap-x-1">
-                            <div className="rounded w-1/2 h-5 bg-slate-200" />
-                            <div className="rounded w-1/2 h-5 bg-slate-200" />
-                        </div>
-                    </div>
-                    <div className="rounded w-full h-12.5 bg-slate-200"></div>
-                </div>
             </div>
         </Card>
 
@@ -57,6 +39,13 @@ export default function SkeletonMarketMaking(props) {
 
             </div>
             <div className="card-content pt-5.5 space-y-5">
+                <div className="space-y-2.5">
+                    <div className="rounded w-1/6 h-8 bg-slate-200" />
+                    <div className="grid grid-cols-2 gap-x-4 gap-y-2.5">
+                        <div className="rounded w-full h-12.5 bg-slate-200" />
+                        <div className="rounded w-full h-12.5 bg-slate-200" />
+                    </div>
+                </div>
                 <div className="grid md-lg:grid-cols-2 gap-5">
                     <div className="flex flex-col space-y-10">
                         <div className="rounded w-1/3 h-5 bg-slate-200" />
@@ -69,24 +58,9 @@ export default function SkeletonMarketMaking(props) {
                 </div>
                 <div className="space-y-2.5">
                     <div className="rounded w-1/6 h-8 bg-slate-200" />
-                    <div className="grid grid-cols-2 gap-x-4 gap-y-2.5">
-                        <div className="rounded w-full h-12.5 bg-slate-200" />
-                        <div className="rounded w-full h-12.5 bg-slate-200" />
-                    </div>
-                </div>
-                <div className="space-y-2.5">
-                    <div className="rounded w-1/6 h-8 bg-slate-200" />
                     <div className="rounded w-full h-12.5 bg-slate-200" />
                 </div>
                 <div className="rounded-full w-full h-10 bg-slate-200" />
-                <div className="flex justify-between">
-                    <div className="rounded w-1/6 h-5 bg-slate-200" />
-                    <div className="flex flex-row w-1/6 gap-x-1">
-                        <div className="rounded w-1/2 h-5 bg-slate-200" />
-                        <div className="rounded w-1/2 h-5 bg-slate-200" />
-                    </div>
-                </div>
-                <div className="rounded w-full h-12.5 bg-slate-200" />
             </div>
         </Card>
     </div>
