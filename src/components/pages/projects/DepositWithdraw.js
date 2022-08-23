7992dfc8561c03e389f8ee22d26b58b25a582cab
bitcooker <dreamhighpassionhigh@gmail.com>
2022-08-23 19:30:17 +0200

    Restyle DepositWithdraw

--- src/components/pages/projects/DepositWithdraw.js
+++ src/components/pages/projects/DepositWithdraw.js
@@ -1,4 +1,5 @@
 import {useEffect, useState} from "react";
+import {useRouter} from "next/router";
 import {ethers} from "ethers";
 
 import helper from "../../../helpers";
@@ -13,7 +14,7 @@ import InputApproveWithIconSubmit from "../../core/Input/InputApproveWithIconSub
 import MaxButton from "./Button/MaxButton";
 import Card from "../projectDetail/Card/Card";
 import SkeletonDepositAndWithdraw from "./Skeleton/SkeletonDepositAndWithdraw";
-import {useRouter} from "next/router";
+import HomeCard from "../../pages/Home/HomeCard";
 
 export default function DepositWithdraw({wallet, project, marketMakingPool, setTab}) {
 
@@ -97,161 +98,192 @@ export default function DepositWithdraw({wallet, project, marketMakingPool, setT
 
     return !load ? <SkeletonDepositAndWithdraw/> : (
         <div className="grid md-lg:grid-cols-2 gap-7.5">
-            <Card title="Settings">
-                {/* Card Header */}
-                <div className="card-header">
-                    <h1 className="text-2xl"><i className="fa-solid fa-plus-circle"/> Deposit</h1>
+            <div className="flex flex-col gap-5">
+                <Card title="Settings">
+                    {/* Card Header */}
+                    <div className="card-header">
+                        <h1 className="text-2xl"><i className="fa-solid fa-plus-circle"/> Deposit</h1>
+                    </div>
+
+
+                    <div className="pt-10 card-content space-y-5">
+                        <div className="space-y-2.5">
+                            <div className="flex flex-row items-center justify-between text-base">
+                                <div>
+                                    <i className="fa-solid fa-coin"/> Cash
+                                </div>
+                                <MaxButton
+                                    balance={pairedTokenWalletBalance}
+                                    handleClick={() => setMax(pairedTokenWalletBalance, setAmountPairTokenToStake)}
+                                />
+                            </div>
+                            <InputApproveWithIconSubmit
+                                id="cash"
+                                name="cash"
+                                type="number"
+                                icon="fa-light fa-circle-plus"
+                                submitName="Deposit"
+                                submitFunction={stakePairedToken}
+                                value={amountPairTokenToStake}
+                                image={marketMakingPool.paired_token_image}
+                                setValue={setAmountPairTokenToStake}
+                                address={marketMakingPool.address}
+                                token={marketMakingPool.paired_token}
+                            />
+                        </div>
+
+                        <div className="space-y-2.5">
+                            <div className="flex flex-row items-center justify-between text-base">
+                                <div>
+                                    <i className="fa-solid fa-coin"/> Token
+                                </div>
+                                <MaxButton
+                                    balance={baseTokenWalletBalance}
+                                    handleClick={() => setMax(baseTokenWalletBalance, setAmountBaseTokenToStake)}
+                                />
+                            </div>
+                            <InputApproveWithIconSubmit
+                                id="token"
+                                name="token"
+                                type="number"
+                                icon="fa-light fa-circle-plus"
+                                submitName="Deposit"
+                                image={project.image}
+                                submitFunction={stakeBaseToken}
+                                value={amountBaseTokenToStake}
+                                setValue={setAmountBaseTokenToStake}
+                                address={marketMakingPool.address}
+                                token={project.token}
+                            />
+                        </div>
+                    </div>
+                </Card>
+
+                <div className="grid grid-cols-1 lg:grid-cols-3 gap-3">
+                    <HomeCard
+                        icon={<i className="fa-solid fa-circle-plus text-2xl text-indigo-500"></i>} 
+                        title="Release Vesting" 
+                        content="Step-by-step guides to setting up your system and installing the library."
+                        handleClick={() => setTab(5)}
+                    />
+
+                    <HomeCard
+                        icon={<i className="fa-solid fa-hands-holding-dollar text-2xl text-indigo-500"></i>} 
+                        title="Deposit LP Tokens" 
+                        content="Step-by-step guides to setting up your system and installing the library."
+                        handleClick={() => router.push('/farms')}
+                    />
+
+                    <HomeCard
+                        icon={<i className="fa-solid fa-sack-dollar text-2xl text-indigo-500"></i>} 
+                        title="Stake In Vault" 
+                        content="Step-by-step guides to setting up your system and installing the library."
+                        handleClick={() => setTab(4)}
+                    />
                 </div>
-
-
-                <div className="pt-10 card-content space-y-5">
-                    <div className="space-y-2.5">
-                        <div className="flex flex-row items-center justify-between text-base">
-                            <div>
-                                <i className="fa-solid fa-coin"/> Cash
-                            </div>
-                            <MaxButton
-                                balance={pairedTokenWalletBalance}
-                                handleClick={() => setMax(pairedTokenWalletBalance, setAmountPairTokenToStake)}
-                            />
-                        </div>
-                        <InputApproveWithIconSubmit
-                            id="cash"
-                            name="cash"
-                            type="number"
-                            icon="fa-light fa-circle-plus"
-                            submitName="Deposit"
-                            submitFunction={stakePairedToken}
-                            value={amountPairTokenToStake}
-                            image={marketMakingPool.paired_token_image}
-                            setValue={setAmountPairTokenToStake}
-                            address={marketMakingPool.address}
-                            token={marketMakingPool.paired_token}
-                        />
+            </div>
+            <div className="flex flex-col gap-5">
+                <Card title="Activity">
+                    {/* Card Header */}
+                    <div className="card-header">
+                        <h1 className="text-2xl"><i className="fa-solid fa-circle-minus"></i> Withdraw</h1>
                     </div>
 
-                    <div className="space-y-2.5">
-                        <div className="flex flex-row items-center justify-between text-base">
-                            <div>
-                                <i className="fa-solid fa-coin"/> Token
+                    <div className="pt-10 card-content space-y-5">
+                        <div className="space-y-2.5">
+                            <div className="flex flex-row items-center justify-between text-base">
+                                <div>
+                                    <i className="fa-solid fa-circle-dollar mr-1"/>
+                                    Cash
+                                </div>
+                                <MaxButton
+                                    balance={amountPairTokenBalance}
+                                    handleClick={() => setMax(amountPairTokenBalance, setAmountPairTokenToWithdraw)}
+                                />
                             </div>
-                            <MaxButton
-                                balance={baseTokenWalletBalance}
-                                handleClick={() => setMax(baseTokenWalletBalance, setAmountBaseTokenToStake)}
+                            <InputWithIconSubmit
+                                id="withdrawCash"
+                                name="withdrawCash"
+                                type="number"
+                                placeholder="Input amount to withdraw"
+                                submitName="Withdraw"
+                                image={marketMakingPool.paired_token_image}
+                                icon="fa-light fa-circle-minus"
+                                value={amountPairTokenToWithdraw}
+                                setValue={setAmountPairTokenToWithdraw}
+                                submitFunction={withdrawPairToken}
                             />
                         </div>
-                        <InputApproveWithIconSubmit
-                            id="token"
-                            name="token"
-                            type="number"
-                            icon="fa-light fa-circle-plus"
-                            submitName="Deposit"
-                            image={project.image}
-                            submitFunction={stakeBaseToken}
-                            value={amountBaseTokenToStake}
-                            setValue={setAmountBaseTokenToStake}
-                            address={marketMakingPool.address}
-                            token={project.token}
-                        />
-                    </div>
-                    <div>
-                        <Button
-                            className={''} name="Release Vesting" handleClick={() => setTab(5)}>
-                            <i className="pl-2 fa-solid fa-circle-plus"/>
-                        </Button>
-                    </div>
-                    <div>
-                        <Button
-                            className={'col-span-full'} name="Deposit LP Tokens"
-                            handleClick={() => router.push('/farms')}>
-                            <i className="pl-2 fa-solid fa-plus-circle"/>
-                        </Button>
-                    </div>
-                    <div>
-                        <Button
-                            className={'col-span-full'} name="Stake In Vault" handleClick={() => setTab(4)}>
-                            <i className="pl-2 fa-solid fa-plus-circle"/>
-                        </Button>
-                    </div>
-                </div>
-            </Card>
-            <Card title="Activity">
-                {/* Card Header */}
-                <div className="card-header">
-                    <h1 className="text-2xl"><i className="fa-solid fa-circle-minus"></i> Withdraw</h1>
-                </div>
-
-                <div className="pt-10 card-content space-y-5">
-                    <div className="space-y-2.5">
-                        <div className="flex flex-row items-center justify-between text-base">
-                            <div>
-                                <i className="fa-solid fa-circle-dollar mr-1"/>
-                                Cash
+                        <div className="space-y-2.5">
+                            <div className="flex flex-row items-center justify-between text-base">
+                                <div>
+                                    <i className="fa-solid fa-coin mr-1"/>
+                                    Tokens
+                                </div>
+                                <MaxButton
+                                    balance={amountBaseTokenBalance}
+                                    handleClick={() => setMax(amountBaseTokenBalance, setAmountBaseTokenToWithdraw)}
+                                />
                             </div>
-                            <MaxButton
-                                balance={amountPairTokenBalance}
-                                handleClick={() => setMax(amountPairTokenBalance, setAmountPairTokenToWithdraw)}
+                            <InputWithIconSubmit
+                                id="withdrawToken"
+                                name="withdrawToken"
+                                type="number"
+                                placeholder="Input amount to withdraw"
+                                submitName="Withdraw"
+                                image={project.image}
+                                icon="fa-light fa-circle-minus"
+                                value={amountBaseTokenToWithdraw}
+                                setValue={setAmountBaseTokenToWithdraw}
+                                submitFunction={withdrawBaseToken}
                             />
                         </div>
-                        <InputWithIconSubmit
-                            id="withdrawCash"
-                            name="withdrawCash"
-                            type="number"
-                            placeholder="Input amount to withdraw"
-                            submitName="Withdraw"
-                            image={marketMakingPool.paired_token_image}
-                            icon="fa-light fa-circle-minus"
-                            value={amountPairTokenToWithdraw}
-                            setValue={setAmountPairTokenToWithdraw}
-                            submitFunction={withdrawPairToken}
-                        />
-                    </div>
-                    <div className="space-y-1">
-                        <div className="flex flex-row items-center justify-between text-base">
-                            <div>
-                                <i className="fa-solid fa-coin mr-1"/>
-                                Tokens
-                            </div>
-                            <MaxButton
-                                balance={amountBaseTokenBalance}
-                                handleClick={() => setMax(amountBaseTokenBalance, setAmountBaseTokenToWithdraw)}
-                            />
-                        </div>
-                        <InputWithIconSubmit
-                            id="withdrawToken"
-                            name="withdrawToken"
-                            type="number"
-                            placeholder="Input amount to withdraw"
-                            submitName="Withdraw"
-                            image={project.image}
-                            icon="fa-light fa-circle-minus"
-                            value={amountBaseTokenToWithdraw}
-                            setValue={setAmountBaseTokenToWithdraw}
-                            submitFunction={withdrawBaseToken}
-                        />
-                    </div>
-                    <div>
-                        <Button
-                            className={'col-span-full'} name="Withdraw Liquidity" handleClick={() => setTab(3)}>
-                            <i className="pl-2 fa-solid fa-circle-minus"/>
-                        </Button>
-                    </div>
-                    <div>
-                        <Button
-                            className={'col-span-full'} name="Withdraw LP Tokens"
-                            handleClick={() => router.push('/farms')}>
-                            <i className="pl-2 fa-solid fa-plus-circle"/>
-                        </Button>
-                    </div>
-                    <div className={'grid grid-cols-1 gap-2.5'}>
-                        <div>
+                        {/* <div>
                             <Button
-                                className={''} name="Withdraw Vault Rewards" handleClick={() => setTab(4)}>
+                                className={'col-span-full'} name="Withdraw Liquidity" handleClick={() => setTab(3)}>
                                 <i className="pl-2 fa-solid fa-circle-minus"/>
                             </Button>
                         </div>
+                        <div>
+                            <Button
+                                className={'col-span-full'} name="Withdraw LP Tokens"
+                                handleClick={() => router.push('/farms')}>
+                                <i className="pl-2 fa-solid fa-plus-circle"/>
+                            </Button>
+                        </div>
+                        <div className={'grid grid-cols-1 gap-2.5'}>
+                            <div>
+                                <Button
+                                    className={''} name="Withdraw Vault Rewards" handleClick={() => setTab(4)}>
+                                    <i className="pl-2 fa-solid fa-circle-minus"/>
+                                </Button>
+                            </div>
+                        </div> */}
                     </div>
+                </Card>
+
+                <div className="grid grid-cols-1 lg:grid-cols-3 gap-3">
+                    <HomeCard
+                        icon={<i className="fa-solid fa-circle-minus text-2xl text-indigo-500"></i>} 
+                        title="Withdraw Liquidity"
+                        content="Step-by-step guides to setting up your system and installing the library."
+                        handleClick={() => setTab(3)}
+                    />
+
+                    <HomeCard
+                        icon={<i className="fa-solid fa-hand-holding-dollar text-2xl text-indigo-500"></i>} 
+                        title="Withdraw LP Tokens"
+                        content="Step-by-step guides to setting up your system and installing the library."
+                        handleClick={() => router.push('/farms')}
+                    />
+
+                    <HomeCard
+                        icon={<i className="fa-solid fa-face-tongue-money text-2xl text-indigo-500"></i>} 
+                        title="Withdraw Vault Rewards"
+                        content="Step-by-step guides to setting up your system and installing the library."
+                        handleClick={() => setTab(4)}
+                    />
                 </div>
-            </Card>
+            </div>
         </div>)
 }
