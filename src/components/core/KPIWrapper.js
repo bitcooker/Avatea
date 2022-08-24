363b953081d8137306ea5ba66642fe7c051c7322
A <albertsamseer@MacBook-Air-van-Albert.local>
2022-08-24 16:15:00 +0200

    Fix tailwind

--- src/components/core/KPIWrapper.js
+++ src/components/core/KPIWrapper.js
@@ -1,6 +1,12 @@
 export default function KPIWrapper(props) {
+    console.log(props);
     return(
-        <div className={`grid md-lg:grid-cols-${props.cols ? props.cols : '3'} gap-7.5`}>
+        <div className={`grid grid-cols-1 gap-7.5
+         ${props.cols === 2 ? 'md-lg:grid-cols-2' : "md-lg:grid-cols-3"}
+         ${props.cols === 4 ? 'md-lg:grid-cols-4' : "md-lg:grid-cols-3"}
+         ${props.cols === 5 ? 'md-lg:grid-cols-5' : "md-lg:grid-cols-3"}
+         ${props.cols === 6 ? 'md-lg:grid-cols-6' : "md-lg:grid-cols-3"}
+         `}>
             {props.children}
         </div>
         )
