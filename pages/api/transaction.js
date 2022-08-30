6b82625bcef20371c464f2e0dbf81cc44bde1431
A <albertsamseer@MacBook-Air-van-Albert.local>
2022-08-30 11:42:30 +0200

    Remove logs

--- pages/api/transaction.js
+++ pages/api/transaction.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import {API_URL} from "../../src/helpers/constants";
 
 export default async function handler(req, res) {
-    console.log(process.env)
     if (req.method === "POST" && req.headers.host === "localhost:3000" || req.headers.host === "staging.avatea.io" || req.headers.host === "app.avatea.io") {
         try {
             await axios.post(`${API_URL}Transaction/`, req.body, {
@@ -12,7 +11,7 @@ export default async function handler(req, res) {
             })
             res.status(200).json({ status: 200 })
         } catch (e) {
-            res.status(500).json({ error: e })
+            res.status(500).json({ error: 'Error, check logs' })
         }
     } else {
         res.status(200).json({status: 'OK'})
