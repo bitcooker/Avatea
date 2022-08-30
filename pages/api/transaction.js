bb2826ed30ba629d0004fb9abefa22ae1d250f89
A <albertsamseer@MacBook-Air-van-Albert.local>
2022-08-30 11:38:40 +0200

    Debug log

--- pages/api/transaction.js
+++ pages/api/transaction.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import {API_URL} from "../../src/helpers/constants";
 
 export default async function handler(req, res) {
+    console.log(process.env)
     if (req.method === "POST" && req.headers.host === "localhost:3000" || req.headers.host === "staging.avatea.io" || req.headers.host === "app.avatea.io") {
         try {
             await axios.post(`${API_URL}Transaction/`, req.body, {
