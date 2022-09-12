b37018f9d74369832e40cc5fe5a6217056759b52
bitcooker <dreamhighpassionhigh@gmail.com>
2022-09-12 19:43:24 +0200

    feat: restyle transaction table

--- src/components/pages/myActivity/Transaction/TransactionItem.jsx
+++ src/components/pages/myActivity/Transaction/TransactionItem.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import moment from 'moment';
 import stc from 'string-to-color'
 
@@ -71,41 +71,70 @@ function parseValue(value, type) {
 }
 
 export default function TransactionItem(props) {
+    const shortenAddress = useCallback((address) => {
+        return address != null
+          ? `${address.slice(0, 6)}...${address.slice(
+              address.length - 4,
+              address.length
+            )}`
+          : "";
+    },[]);
+
     return (
-        <div className="grid grid-cols-12 items-center w-full p-2">
-            <TableCol className="truncate col-span-4 font-medium text-base tracking-[.0125rem]">
-                <div className={`inline-block px-3 py-2 rounded-full border-2 border-opacity-50 border-gray-300 text-black hover:bg-gray-300 delay-150 transition-all hover:border-opacity-100 hover:cursor-pointer text-center`}>
-                    <div className="hidden md-lg:flex items-center gap-2 text-sm leading-none">
-                        {props.image ?
-                            <Image src={props.image} alt="" width={16} height={16}/>
-                            : ''
-                        }
-                        {TRANSACTION_TYPES[props.type]}
-                    </div>
-                    <p className="md-lg:hidden text-sm leading-none">{props.type}</p>
-                </div>
-            </TableCol>
-            <TableCol className="truncate col-span-3 font-medium text-base tracking-[.0125rem]">
-                <small>
-                    <i className="fa-solid fa-timer mr-1"></i>
-                    <span className="hidden md-lg:inline-block">{moment(props.timestamp).format('llll')}</span>
-                    <span className="md-lg:hidden">{moment(props.timestamp).fromNow()}</span>
-                </small>
-            </TableCol>
-            <TableCol className="col-span-4 flex justify-center md-lg:justify-start font-medium text-base tracking-[.0125rem] text-white">
-                <div className={`text-slate-600 text-sm`}>
-                    {props.amount > 0 ? props.amount :
+        // <div className="grid grid-cols-12 items-center w-full p-2">
+        //     <TableCol className="truncate col-span-4 font-medium text-base tracking-[.0125rem]">
+        //         <div className={`inline-block px-3 py-2 rounded-full border-2 border-opacity-50 border-gray-300 text-black hover:bg-gray-300 delay-150 transition-all hover:border-opacity-100 hover:cursor-pointer text-center`}>
+        //             <div className="hidden md-lg:flex items-center gap-2 text-sm leading-none">
+        //                 {props.image ?
+        //                     <Image src={props.image} alt="" width={16} height={16}/>
+        //                     : ''
+        //                 }
+        //                 {TRANSACTION_TYPES[props.type]}
+        //             </div>
+        //             <p className="md-lg:hidden text-sm leading-none">{props.type}</p>
+        //         </div>
+        //     </TableCol>
+        //     <TableCol className="truncate col-span-3 font-medium text-base tracking-[.0125rem]">
+        //         <small>
+        //             <i className="fa-solid fa-timer mr-1"></i>
+        //             <span className="hidden md-lg:inline-block">{moment(props.timestamp).format('llll')}</span>
+        //             <span className="md-lg:hidden">{moment(props.timestamp).fromNow()}</span>
+        //         </small>
+        //     </TableCol>
+        //     <TableCol className="col-span-4 flex justify-center md-lg:justify-start font-medium text-base tracking-[.0125rem] text-white">
+        //         <div className={`text-slate-600 text-sm`}>
+        //             {props.amount > 0 ? props.amount :
+        //                 parseValue(props.value, props.type)
+        //             }
+        //         </div>
+        //     </TableCol>
+        //     <TableCol className="col-span-1 flex items-center justify-center">
+        //         <a target={'_blank'} href={`https://rinkeby.etherscan.io/tx/${props.hash}`} rel={'noreferrer'} className={'hover:cursor-pointer'}>
+        //             <div className={`hover:cursor-pointer flex items-center justify-center w-6 h-6 pt-[1.5px] rounded-md border border-slate-600/50 hover:border-slate-600`}>
+        //                 <i className={`cursor-pointer fa-solid fa-eye fa-xs text-slate-600`}/>
+        //             </div>
+        //         </a>
+        //     </TableCol>
+        // </div>
+        <div className="flex items-center justify-between p-3">
+            <div className="flex flex-col gap-1">
+                <span className="text-lg">
+                    {TRANSACTION_TYPES[props.type]}
+                </span>
+                <span className="text-slate-500"> <i className="fa-light fa-calendar" /> Closed on {moment(props.timestamp).fromNow()} </span>
+            </div>
+
+            <div className="flex flex-col">
+                <div className="flex justify-end text-right text-green-600">
+                    {props.amount > 0 ? <span className="px-1 rounded-md bg-green-100">${props.amount}</span> :
                         parseValue(props.value, props.type)
                     }
                 </div>
-            </TableCol>
-            <TableCol className="col-span-1 flex items-center justify-center">
-                <a target={'_blank'} href={`https://rinkeby.etherscan.io/tx/${props.hash}`} rel={'noreferrer'} className={'hover:cursor-pointer'}>
-                    <div className={`hover:cursor-pointer flex items-center justify-center w-6 h-6 pt-[1.5px] rounded-md border border-slate-600/50 hover:border-slate-600`}>
-                        <i className={`cursor-pointer fa-solid fa-eye fa-xs text-slate-600`}/>
-                    </div>
+                <a target="_blank" href={`https://rinkeby.etherscan.io/tx/${props.hash}`} rel={'noreferrer'} className="text-sky-500 cursor-pointer hover:underline">
+                    <span className="hidden md-lg:block">{props.hash}</span>
+                    <span className="block md-lg:hidden">{shortenAddress(props.hash)}</span>
                 </a>
-            </TableCol>
+            </div>
         </div>
     );
 }
--- src/components/pages/myActivity/Transaction/TransactionWrapper.jsx
+++ src/components/pages/myActivity/Transaction/TransactionWrapper.jsx
@@ -1,15 +1,19 @@
 import React, {useEffect, useState} from "react";
+import {useWallet} from "@albs1/use-wallet";
+
+// core components
+import Spinner from "../../../core/Spinner";
+
+// page components
 import TransactionItem from "./TransactionItem";
 import { ActModul } from "./ActModul";
-import {useWallet} from "@albs1/use-wallet";
 import helpers from "../../../../helpers";
-import Spinner from "../../../core/Spinner";
 import user from "../../../../helpers/user";
 
 export default function TransactionWrapper( { userAddress,projectSlug }) {
-
     const wallet = useWallet();
     const [transactions,setTransactions] = useState(null);
+    const [page, setPage] = useState(0);
 
     useEffect(() => {
         if(wallet.status === "connected") {
@@ -41,15 +45,57 @@ export default function TransactionWrapper( { userAddress,projectSlug }) {
     return (
     <div className="grow p-5 rounded-2.5xl bg-white">
         <h2 className="text-2xl">Transactions</h2>
-        <div className="w-full py-5 h-[550px] overflow-hidden hover:scrollbar-thin hover:scrollbar-thumb-gray-300">
+        <div className="w-full py-5 overflow-hidden hover:scrollbar-thin hover:scrollbar-thumb-gray-300 divide-y">
         {
-            transactions ? transactions?.map((transaction) => {
+            transactions ? transactions?.slice(0, 10).map((transaction) => {
                 return <TransactionItem key={transaction.id} {...transaction} />;
             }) : <div className={'flex h-full items-center justify-center'}>
                 <Spinner size={5}/>
             </div>
         }
-      </div>
+        </div>
+        <div className="flex items-center justify-between border-t border-gray-200 bg-white px-1 py-3 sm:px-2">
+            <div className="flex flex-1 justify-between sm:hidden">
+                <a href="#" className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Previous</a>
+                <a href="#" className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Next</a>
+            </div>
+            <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+                <div>
+                    <p className="text-sm text-gray-700">
+                        Showing 
+                        <span className="font-medium"> 1 </span>
+                        to 
+                        <span className="font-medium"> 10 </span>
+                        of 
+                        <span className="font-medium"> 97 </span>
+                        transactions
+                    </p>
+                </div>
+                <div>
+                <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+                    <a href="#" className="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20">
+                    <span className="sr-only">Previous</span>
+                    <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fillRule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clipRule="evenodd" />
+                    </svg>
+                    </a>
+                    <a href="#" aria-current="page" className="relative z-10 inline-flex items-center border border-indigo-500 bg-indigo-50 px-4 py-2 text-sm font-medium text-indigo-600 focus:z-20">1</a>
+                    <a href="#" className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20">2</a>
+                    <a href="#" className="relative hidden items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 md:inline-flex">3</a>
+                    <span className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700">...</span>
+                    <a href="#" className="relative hidden items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 md:inline-flex">8</a>
+                    <a href="#" className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20">9</a>
+                    <a href="#" className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20">10</a>
+                    <a href="#" className="relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20">
+                    <span className="sr-only">Next</span>
+                    <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
+                    </svg>
+                    </a>
+                </nav>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
